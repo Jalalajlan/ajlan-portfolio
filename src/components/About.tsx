e364f40be7f8df4ef4a5b6e03752cff08dccefbf
jalalajlan <jalalajlan@gmail.com>
2024-01-07 21:54:23 +0900

    Updated about component to include localization.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,29 +1,20 @@
 import useDeviceDetection from "../hooks/useDeviceDetection";
+import useTranslation from "../hooks/useTranslation";
 import "../styles/About.css";
 
 const About: React.FC = () => {
+  const translation = useTranslation();
   const isMobile = useDeviceDetection();
 
   return (
     <div id="about">
-      {isMobile ? <h3 className="about-title">ABOUT</h3> : null}
-      <p className="about-description">
-        From a young age, I have always had a passion for coding, fueled by the
-        wonder of how the internet works. I used to play with custom Google blog
-        templates using HTML and CSS and it was really fun. Fast-forwarded to
-        today, and I turned my passion into success in building production
-        software.
-        <br />
-        <br />I have a <b> degree in Software Engineering </b> backed up by{" "}
-        <b>1.4 years of practical experience</b> with a focus on building
-        robust, high-quality software and solving business problems. I've used a
-        variety of programming languages including{" "}
-        <b> JavaScript, Java, Dart, and PHP.</b>
-        <br />
-        <br />
-        When I'm away from the computer, you'll find me at the gym, hanging out
-        with my brother or reading a book.
-      </p>
+      {isMobile ? (
+        <h3 className="about-title">{translation.about.toUpperCase()}</h3>
+      ) : null}
+      <p
+        className="about-description"
+        dangerouslySetInnerHTML={{ __html: translation.aboutDescription }}
+      ></p>
     </div>
   );
 };
--- src/localization/en.ts
+++ src/localization/en.ts
@@ -5,4 +5,6 @@ export default {
   about: "about",
   experience: "experience",
   projects: "projects",
+  aboutDescription:
+    " From a young age, I have always had a passion for coding, fueled by the wonder of how the internet works. I used to play with custom Google blog templates using HTML and CSS and it was really fun. Fast-forwarded to today, and I turned my passion into success in building production software.<br /> <br /> I have a <b> degree in Software Engineering </b> backed up by <b>1.4 years of practical experience</b> with a focus on building robust, high-quality software and solving business problems. I've used a variety of programming languages including<b> JavaScript, Java, Dart, and PHP.</b> <br /><br />When I'm away from the computer, you'll find me at the gym, hanging out with my brother or reading a book.",
 };
